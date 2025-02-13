419a924140011a94f8bc3e70d3a8256f00f3cc2e
Sambita Mondal <156224126+thunderrainstorm@users.noreply.github.com>
2025-02-14 04:15:07 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -11,6 +11,7 @@ import './App.css';
 const API_URL = process.env.NODE_ENV === 'production' 
   ? 'https://codefix-3.onrender.com'  
   : 'http://localhost:3000';
+export default API_URL;
 
 function App() {
   // Load DRAFT from localStorage
