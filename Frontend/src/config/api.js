eae7dd2f10aa9f376741a2a521e7a9e23370a678
Sambita Mondal <156224126+thunderrainstorm@users.noreply.github.com>
2025-02-14 04:27:57 +0530

    Delete Frontend/src/config directory

--- Frontend/src/config/api.js
+++
@@ -1,6 +0,0 @@
-// src/config/api.js or similar
-const API_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://codefix-3.onrender.com'  
-  : 'http://localhost:3000';
-
-export default API_URL;
